bbb4d9e41ea2eed47dc1411ef9eeea5a9c50b8d5
Nazmul Ahsan <n.mukto@gmail.com>
2020-07-30 22:59:21 +0600

    license text fix

--- cx-plugin.php
+++ cx-plugin.php
@@ -9,12 +9,12 @@
  * Text Domain: cx-plugin
  * Domain Path: /languages
  *
- * Woolementor is free software: you can redistribute it and/or modify
+ * CX_Plugin is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * any later version.
  *
- * Woolementor is distributed in the hope that it will be useful,
+ * CX_Plugin is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
