8880e4a01d223001033b9f38a9ca43006773a6c6
Nazmul Ahsan <n.mukto@gmail.com>
2020-09-06 13:49:16 +0600

    misc fix

--- product/Update.php
+++ product/Update.php
@@ -19,20 +19,18 @@ class Update extends Base {
 
 	public function __construct( $plugin ) {
 
-		require dirname(__FILE__) . '/update/Factory.php';
-		require dirname(__FILE__) . '/update/Autoloader.php';
+		require dirname( __FILE__ ) . '/update/Factory.php';
+		require dirname( __FILE__ ) . '/update/Autoloader.php';
 		new \Puc_v4p5_Autoloader();
 
 		\Puc_v4p5_Factory::addVersion( 'Plugin_UpdateChecker', 'Puc_v4p5_Plugin_UpdateChecker', '4.5' );
 
-		$this->plugin = $plugin;
+		$this->plugin	= $plugin;
 		
-		$this->slug = $this->plugin['TextDomain'];
-		$this->server = $this->plugin['Server'];
+		$this->slug		= $this->plugin['TextDomain'];
+		$this->server	= $this->plugin['Server'];
 
-		$update = \Puc_v4p5_Factory::buildUpdateChecker(
-			"{$this->server}/wp-products/?action=get_metadata&slug={$this->slug}", $this->plugin['File'], $this->slug
-		);
+		$update = \Puc_v4p5_Factory::buildUpdateChecker( "{$this->server}/wp-products/?action=get_metadata&slug={$this->slug}", $this->plugin['File'], $this->slug );
 
 		$this->filter( 'plugins_api_result', 'set_download_link', 10, 3 );
 	}
