dd700b7361770cbf728a0ad81ac956cd9688fd32
Nazmul Ahsan <n.mukto@gmail.com>
2022-01-10 13:00:14 +0600

    cleanup

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,23 +1,13 @@
 jQuery(function($){
 	$('.cx-plugin-help-heading').click(function(e){
 		var $this = $(this);
-		var target = $this.data('target');
-		$('.cx-plugin-help-text:not('+target+')').slideUp();
-		if($(target).is(':hidden')){
-			$(target).slideDown();
+		var $target = $this.data('target');
+		$('.cx-plugin-help-text:not('+$target+')').slideUp();
+		if($($target).is(':hidden')){
+			$($target).slideDown();
 		}
 		else {
-			$(target).slideUp();
+			$($target).slideUp();
 		}
 	});
-
-	$('.cx_plugin_help_tablinks .cx_plugin_help_tablink').on( 'click', function(e){
-        e.preventDefault();
-        var tab_id = $(this).attr('id');
-        $('.cx_plugin_help_tablink').removeClass('active');
-        $(this).addClass('active');
-
-        $('.cx_plugin_tabcontent').hide();
-        $('#'+tab_id+'_content').show();
-    } );
 })
