a0af2f48c881b9733f107767524f6ef01088a57c
Nazmul Ahsan <n.mukto@gmail.com>
2022-01-20 08:37:40 +0600

    txt

--- uninstall.php
+++ uninstall.php
@@ -8,5 +8,7 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
     exit;
 }
 
-delete_option( 'cx-plugin_version' );
-delete_option( 'cx-plugin_install_time' );
+$deletable_options = [ 'cx-plugin_version', 'cx-plugin_install_time', 'cx-plugin_docs_json', 'codexpert-blog-json' ];
+foreach ( $deletable_options as $option ) {
+    delete_option( $option );
+}
