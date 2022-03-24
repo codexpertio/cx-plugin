cccf5589d9da84343cb64b9a794aa6263894461f
Nazmul Ahsan <n.mukto@gmail.com>
2022-03-24 15:34:09 +0600

    tools tab

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -10,4 +10,18 @@ jQuery(function($){
 			$($target).slideUp();
 		}
 	});
+
+	$('#cx-plugin_report-copy').click(function(e) {
+		e.preventDefault();
+		$('#cx-plugin_tools-report').select();
+
+		try {
+			var successful = document.execCommand('copy');
+			if( successful ){
+				$(this).html('<span class="dashicons dashicons-saved"></span>');
+			}
+		} catch (err) {
+			console.log('Oops, unable to copy!');
+		}
+	});
 })
--- src/Settings.php
+++ src/Settings.php
@@ -27,6 +27,15 @@ class Settings extends Base {
 	
 	public function init_menu() {
 		
+		$site_config = [
+			'PHP Version'				=> PHP_VERSION,
+			'WordPress Version' 		=> get_bloginfo( 'version' ),
+			'WooCommerce Version'		=> is_plugin_active( 'woocommerce/woocommerce.php' ) ? get_option( 'woocommerce_version' ) : 'Not Active',
+			'Memory Limit'				=> defined( 'WP_MEMORY_LIMIT' ) && WP_MEMORY_LIMIT ? WP_MEMORY_LIMIT : 'Not Defined',
+			'Debug Mode'				=> defined( 'WP_DEBUG' ) && WP_DEBUG ? 'Enabled' : 'Disabled',
+			'Active Plugins'			=> get_option( 'active_plugins' ),
+		];
+
 		$settings = [
 			'id'            => $this->slug,
 			'label'         => $this->name,
@@ -468,6 +477,30 @@ class Settings extends Base {
 								],
 							]
 						]
+					]
+				],
+				'cx-plugin_tools'	=> [
+					'id'        => 'cx-plugin_tools',
+					'label'     => __( 'Tools', 'cx-plugin' ),
+					'icon'      => 'dashicons-hammer',
+					'fields'    => [
+						'enable_debug' => [
+							'id'      	=> 'enable_debug',
+							'label'     => __( 'Enable Debug', 'woolementor' ),
+							'type'      => 'switch',
+							'desc'      => __( 'Enable this if you face any CSS or JS related issues.', 'woolementor' ),
+							'disabled'  => false,
+						],
+						'report' => [
+							'id'      => 'report',
+							'label'     => __( 'Report', 'woolementor' ),
+							'type'      => 'textarea',
+							'desc'     	=> '<button id="cx-plugin_report-copy" class="button button-primary"><span class="dashicons dashicons-admin-page"></span></button>',
+							'columns'   => 24,
+							'rows'      => 10,
+							'default'   => json_encode( $site_config, JSON_PRETTY_PRINT ),
+							'readonly'  => true,
+						],
 					]
 				],
 				'cx-plugin_table' => [
