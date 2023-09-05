12adff165c9aa68b7adde9a80e5b233c32b86ad5
Nazmul Ahsan <n.mukto@gmail.com>
2023-09-05 21:00:50 +0600

    misc

---
+++ vendor/composer/installed.php
@@ -0,0 +1,56 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '51669f8838719b651226140e1a433f9c4f994066',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '51669f8838719b651226140e1a433f9c4f994066',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'codexpert/plugin' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '44741326a802312d57b88b1b5d7bd04a80252ed8',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../codexpert/plugin',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+        'pluggable/marketing' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'e4bad013de84f1ca6d99f5b0eacc8d9f8d77f89c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../pluggable/marketing',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+        'pluggable/plugin' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '0a5bd62b9a5f1228f56bad30acbf260f1f14967c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../pluggable/plugin',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+    ),
+);
